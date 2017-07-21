3e5c15a9722b8b080871eb2283ba175b73d0b286
Filip Parag <filiparag.com@gmail.com>
2017-07-21 22:06:26 +0200

    Noise fix

--- Simulation/IMU_Noise.m
+++ Simulation/IMU_Noise.m
@@ -1,24 +1,22 @@
-function [ o_signal ] = IMU_Noise( i_signal, i_cb, i_rw, i_fn )
-
-    Length = size(i_signal, 2);
+function [ o_signal, o_current_state ] = IMU_Noise( i_signal, i_cb, i_rw, i_fn, i_previous_state )
 
     % Constant Bias
     Constant_bias = i_cb;
-    o_signal = i_signal + (Constant_bias / Length : Constant_bias / Length : Constant_bias);
+    o_current_state(1) = Constant_bias + i_previous_state(1);
+    o_signal = i_signal + o_current_state;
 
     % Random Walk
     Random_walk_step = i_rw;
-    Random_walk_current_location = 0;
-    for Index = 1 : Length
-        Random_walk_current_location = Random_walk_current_location + ...
-                                       ((rand(1) > 0.5)*2 - 1) * Random_walk_step * 2 / 8 + ...
-                                       (o_signal(1, Index) < 0) * Random_walk_step * 6 / 8;
-        o_signal(1, Index) = o_signal(1, Index) + Random_walk_current_location;
-    end
+    Random_walk_current_location = i_previous_state(2);
+    Random_walk_current_location = Random_walk_current_location + ...
+                                   ((rand(1) > 0.5)*2 - 1) * Random_walk_step * 2 / 8 + ...
+                                   (o_signal < 0) * Random_walk_step * 6 / 8;
+    o_signal = o_signal + Random_walk_current_location;
+    o_current_state(2) = Random_walk_current_location;
 
     % Flicker noise
     Flicker_noise_power = i_fn;
-    o_signal = o_signal + wgn(1, Length, Flicker_noise_power);
+    o_signal = o_signal + wgn(1, 1, Flicker_noise_power);
 
 end
 
