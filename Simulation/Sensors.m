cead6e42a3f17163622ab495d158516202144588
Filip Parag <filiparag.com@gmail.com>
2017-07-21 22:53:33 +0200

    Fixes

--- Simulation/Sensors.m
+++ Simulation/Sensors.m
@@ -3,19 +3,16 @@ function [ o_angle, o_current_states ] = ...
          i_previous_states, i_previous_angle)
 
     Gravity = 9.8;
-    Rotary_encoder_steps = 2000;
 
     % Calculate ideal values
     Angular_velocity = i_lean_d1;
     Linear_acceleration = Gravity * sin(i_lean);
-    Rotation_angle_steps = i_wheel_rotation / Rotary_encoder_steps;
     
     % Add noise
     [Angular_velocity, o_current_states(1, :)] = IMU_Noise(Angular_velocity, 0.1, 0.05, 0.01, i_previous_states(1, :));
     [Linear_acceleration, o_current_states(2, :)] = IMU_Noise(Linear_acceleration, 0, 0.15, 0.05, i_previous_states(2, :));
-    Rotation_angle_steps = Encoder_Noise(Rotation_angle, 5);
 
     % Get angle using complementary filter
-    o_angle = Complementary_Filter(Angular_velocity, Linear_acceleration, [Rotation_angle_steps, Rotary_encoder_steps], i_previous_angle);
+    o_angle = Complementary_Filter(Angular_velocity, Linear_acceleration, i_previous_angle);
     
 end
