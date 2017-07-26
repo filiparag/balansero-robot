2f399c9b9791ba3485fe89f70007a7bce7f6f861
Filip Parag <filiparag.com@gmail.com>
2017-07-26 22:45:32 +0200

    Kalman filter

--- Simulation/Kalman_filter.m
+++ Simulation/Kalman_filter.m
@@ -1,64 +1,35 @@
-function [ o_state_estimate, o_error_estimate ] = Kalman_filter( i_control, i_measurement, ...
-           i_state_previous, i_error_previous, i_transfer_function_vehicle, i_transfer_function_actuator )
+function [ Xn, Pn, A, B, H, Q, R ] = Kalman_filter( Un, Zn, Xp, Pp, A, B, H, Q, R )
 
-    %% Input definition
-    % i_control = [wheel_angle_difference]
-    % i_measurement = [gyroscope_angle, acelerometer_angle]
- 
-    %% Constants
-    
-    % Coverting transfer function of vehicle dynamics to state space
-    State_transition_matrix = Kalman_filter_tf2ss(i_transfer_function_vehicle);
-
-    % Coverting transfer function of actuator dynamics to state space
-    Control_matrix = Kalman_filter_tf2ss(i_transfer_function_actuator);
-
-    % Complementary filter for both sensors
-    Observation_matrix = [
-        0.25 0.25 0.25 0.25 ;
-        0    0    0    0    ;
-        0    0    0    0    ;
-        0    0    0    0    ;
-    ];
-
-    Estimated_process_error_covariance = [
-        0   0   0   0   ;
-        0   0   0   0   ;
-        0   0   0   0   ;
-        0   0   0   0   ;
-    ];
-
-    Estimated_measurement_error_covariance = [
-        0.1 0   0   0   ;
-        0   0.1 0   0   ;
-        0   0   0.1 0   ;
-        0   0   0   0.1 ;
-    ];
+    %% Vectors 
+    %  Un   Control vector
+    %  Zn   Measurement vector
+    %  Xn   Estimated current state
+    %  Pn   Estimated average error
+    %  X    Predicted current state
+    %  P    Predicted current covariance
+    %  Xp   Previuos state
+    %  Pp   Previuos average error
+    %  Y    Innovation
+    %  S    Innovation covariance
+    %  K    Kalman gain
+    %  A    State transition matrix
+    %  B    Control matrix
+    %  H	Observation matrix
+    %  Q	Estimated process error covariance
+    %  R	Estimated measurement error covariance
     
     %% Prediction
+    X = A * Xp + B * Un;
+    P = A * Pp * A.' + Q;
     
-    Predicted_state_estimate = State_transition_matrix * i_measurement + Control_matrix * i_control;
+    %% Observation
+    Y = Zn - H * X;
+    S = H * P * H.' + R;
     
-    %Predicted_probability_estimate = (State_transition_matrix * i_state_previous).' * ...
-    Predicted_probability_estimate = (State_transition_matrix * i_state_previous).' * ...
-                                     State_transition_matrix.' + Estimated_process_error_covariance;
-                                 
-    %% Innovation
+    %% Update step
+    K = P * H.' * inv(S);
+    Xn = X + K * Y;
+    Pn = (eye(size(P)) - K * H) * P;
     
-    size(Observation_matrix)
-    size(Predicted_state_estimate)
-    Innovation = i_measurement - Observation_matrix * Predicted_state_estimate;
-    
-    Innovation_covariance = Observation_matrix * Predicted_probability_estimate * ...
-                            Observation_matrix.' + Estimated_measurement_error_covariance;
-                         
-    %% Update
-    
-    Kalman_gain = Predicted_probability_estimate * Observation_matrix.' * inv(Innovation_covariance);
-    
-    o_state_estimate = Predicted_state_estimate + Kalman_gain * Innovation;
-    
-    o_error_estimate = (eye(size(i_error_previous, 1)) * Kalman_gain * Observation_matrix) * o_state_estimate;
-
 end
 
